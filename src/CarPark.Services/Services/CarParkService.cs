ab227efe189487a03dc0af971f2550d2225166bf
David Chen <davidchen@dyntechsolution.biz>
2019-03-03 22:11:59 +0800

    removed some comments

--- src/CarPark.Services/Services/CarParkService.cs
+++ src/CarPark.Services/Services/CarParkService.cs
@@ -74,29 +74,17 @@ namespace CarPark.Services.Services
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects).
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
 
                 disposedValue = true;
             }
         }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~CarParkService() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
 
         // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
 
             Console.WriteLine($"{this.GetType().Name} is disposed.");
         }
@@ -105,7 +93,7 @@ namespace CarPark.Services.Services
         {
             if (disposedValue)
             {
-                throw new Exception($"{this.GetType().Name} is disposed.");
+                throw new ObjectDisposedException($"{this.GetType().Name} is disposed.");
             }
         }
         #endregion
